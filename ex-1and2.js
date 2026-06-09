7709d762b614613dd0f03d8687289f86fdfe1579
korathat789-stack <korathat789@gmail.com>
2026-06-10 03:00:00 +0700

    feat: answer java cariable assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,31 @@
 //Exercise #1: Variable Creation
-//Start coding here
+
+const myName = "Karter";
+console.log(myName);
+
+const mySurname = "Anderson";
+console.log(mySurname);
+
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+console.log(address);
+
+const restaurantName = "Chef's Table by Chef James";
+console.log(restaurantName);
+
+let totalPrice = 15000;
+console.log(totalPrice);
+
+let totalBranch = 1500;
+console.log(totalBranch);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = '(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641'
+console.log(address);
+
+totalPrice = 5000;
+console.log(totalPrice);
+
+totalBranch = 2500;
+console.log(totalBranch);
